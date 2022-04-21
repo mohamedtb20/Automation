4b6a35fe9dbd041cb9ee18c1b3daf078b6a13028
mohamedtb20 <93258392+mohamedtb20@users.noreply.github.com>
2022-04-22 05:29:10 +0800

    Add files via upload

---
+++ Linkedin Automation/auto connect linkedin.py
@@ -0,0 +1,66 @@
+import time
+# selenium is an automation library
+# webdriver is the path to the webbrowser which we can work with
+from selenium import webdriver
+# we define chrome with webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+
+# we give our driver chrome as a webdriver
+s = Service('C:\\Users\\DELL\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe')
+driver: WebDriver = webdriver.Chrome(service=s)
+# opening the driver and enter to our linkedin
+# open the linkedin website waiting 2 seconds and then login
+driver.get('https://www.linkedin.com')
+time.sleep(2)
+# we select the part that we will fill it and interact with
+# will go to the driver in linkdin page and will find the username and the password field to fill
+# we locate our element by xpath
+username = driver.find_element_by_xpath("//input[@name='session_key']")
+password = driver.find_element_by_xpath("//input[@name='session_password']")
+# filling the informations
+username.send_keys('mohammed.tiba@ump.ac.ma')
+password.send_keys('hamid0000')
+time.sleep(2)
+# click on submit
+submit = driver.find_element_by_xpath("//button[@type='submit']").click()
+# giving our driver the link that we will interract with i mean the list which we want to connect with
+driver.get(
+    "https://www.linkedin.com/search/results/people/?keywords=cyber%20security&network=%5B%22S%22%5D&origin=FACETED_SEARCH&sid=2-u")
+time.sleep(2)
+
+
+# we define a function which give us the possibility to repeat the same action a lot of times
+def repeat():
+    # we define a list of a web element definded by tag name : button
+    all_buttons: list[WebElement] = driver.find_elements_by_tag_name("button")
+    # our button connect is the same type of all_buttons with a text "Connect"
+    connect_buttons = [btn for btn in all_buttons if btn.text == "Connect"]
+    for btn in connect_buttons:
+        driver.execute_script("arguments[0].click();", btn)
+        time.sleep(2)
+        # we define and click on other button which appear when clicking on connect button
+        send = driver.find_element_by_xpath("//button[@aria-label='Send now']")
+        driver.execute_script("arguments[0].click();", send)
+        time.sleep(2)
+        close = driver.find_element_by_xpath("//button[@aria-label='Dismiss']")
+        driver.execute_script("arguments[0].click();", close)
+        time.sleep(10)
+    # in the case of the existence of just follow button not a connect button
+    follow_buttons = [btn2 for btn2 in all_buttons if btn2.text == "Follow"]
+    for btn2 in follow_buttons:
+        driver.execute_script("arguments[0].click();", btn2)
+        time.sleep(10)
+    # we scroll down to find the element that permit us to skipe to the next page
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+    time.sleep(10)
+    # the next page button
+    Next_button = driver.find_element_by_xpath("//button[@aria-label='Next']")
+    driver.execute_script("arguments[0].click();", Next_button)
+    time.sleep(5)
+
+
+# we can a fix a time to do that or doing like what i do by execute until not finiding result
+while True:
+    repeat()
---
+++ Twitter Automation/auto follow twitter.py
@@ -0,0 +1,55 @@
+#Twitter auto Follow
+
+import time
+# selenium is an automation library
+# webdriver is the path to the webbrowser which we can work with
+from selenium import webdriver
+# we define chrome with webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
+
+# we give our driver chrome as a webdriver
+s = Service('C:\\Users\\DELL\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe')
+driver: WebDriver = webdriver.Chrome(service=s)
+# opening the driver and enter to our twitter
+# open the twitter website waiting 2 seconds and then login
+driver.get('https://twitter.com/i/flow/login')
+time.sleep(2)
+# we select the part that we will fill it and interact with
+# will go to the driver in twitter page and will find the username and the password field to fill
+# we locate our element by name
+username = driver.find_element_by_name("text")
+# filling the informations
+username.send_keys('MohammedTiba8')
+time.sleep(5)
+# click on Next button we use the css selector with the class
+Next = driver.find_element_by_css_selector(
+    'div.css-18t94o4.css-1dbjc4n.r-sdzlij.r-1phboty.r-rs99b7.r-ywje51.r-usiww2.r-2yi16.r-1qi8awa.r-1ny4l3l.r-ymttw5.r-o7ynqc.r-6416eg.r-lrvibr.r-13qz1uu')
+Next.click()
+time.sleep(2)
+password = driver.find_element_by_name("password")
+password.send_keys('hamid0000')
+time.sleep(2)
+# click on log in
+log = driver.find_element_by_css_selector('div.css-1dbjc4n.r-pw2am6')
+log.click()
+#our url of the list of X that we want to follow
+driver.get(
+    "https://twitter.com/search?q=ceo%20startup&src=typed_query&f=user")
+time.sleep(5)
+
+
+def repeat():
+    # we define a list of a web element definded by tag name : button
+    all_buttons: list[WebElement] = driver.find_elements_by_xpath("//button[@role='button']")
+    #we define the follow button
+    follow_buttons = [btn2 for btn2 in all_buttons if btn2.text == "Follow"]
+    for btn2 in follow_buttons:
+        driver.execute_script("arguments[0].click();", btn2)
+        time.sleep(10)
+
+# we can a fix a time to do that or doing like what i do by execute until not finiding result
+
+while True:
+    repeat()
